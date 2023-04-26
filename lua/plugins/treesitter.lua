4965439acdcaf303a89e486099e58760e08e9777
Jonathon Adams <24870903+jonathonadams@users.noreply.github.com>
2023-04-26 11:28:40 +0930

    change treesitter help to vimdoc

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -8,7 +8,7 @@ return {
     config = function()
         require('nvim-treesitter.configs').setup {
             -- A list of parser names, or "all"
-            ensure_installed = { "help", "javascript", "typescript",  "python", "lua", "rust", "go" },
+            ensure_installed = { "vimdoc", "javascript", "typescript",  "python", "lua", "rust", "go" },
 
             -- Install parsers synchronously (only applied to `ensure_installed`)
             sync_install = false,
@@ -21,7 +21,7 @@ return {
             highlight = {
                 -- `false` will disable the whole extension
                 enable = true,
-        
+                
                 -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
                 -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
                 -- Using this option may slow down your editor, and you may see some duplicate highlights.
